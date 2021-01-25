657efd401b1c521e5a9c9baba27d41c2a3346670
ampeiffer <75280957+ampeiffer@users.noreply.github.com>
2021-01-25 18:27:19 -0500

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  1=no closure, 2=closure used
   2. Which of the two uses a closure? How can you tell?
-  
+  counter 2 because the variable is listed outside the function
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better?  counter 1 might be preferable if the variable could be picked up by another aspect of your code, counter 2 might be preferable if you use a similar function several times and need that variable on those occasions.
 */
 
 // counter1 code
@@ -62,8 +62,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.floor(Math.random() * Math.floor(3));
 }
 
 
@@ -81,8 +81,14 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning, innings){
+  let currsumH = 0;
+  let currsumA = 0;
+  for(let i=0;i<=innings;i++){
+    currsumH += inning();
+    currsumA += inning();
+  }
+  return {Away: currsumA, Home: currsumH};
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +96,9 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inning) {
+
+return {Away: inning(), Home: inning()};
 }
 
 
@@ -136,9 +143,28 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
-}
+ function scoreboard(getInningScore, inning, num) {
+  let emptarr = [];
+  let homeScore = 0;
+  let awayScore = 0;
+  let homeTotal = 0;
+  let awayTotal = 0;
+  for(let i=0; i<num; i++){
+    homeTotal = inning(i);
+    homeScore += homeTotal;
+    awayTotal = inning(i);
+    awayScore += awayTotal;
+    emptarr.push(`Inning ${i+1}: Away ${awayTotal} - Home ${homeTotal}`)
+  }
+  if(homeScore === awayScore){
+    emptarr.push(`This game will require extra innings: Away ${awayScore} - Home ${homeScore}`);
+    return  emptarr;
+  }
+  else{
+    emptarr.push(`Final Score: Away ${awayScore}  - Home ${homeScore}`);
+    return emptarr;
+  }
+};
 
 
 
